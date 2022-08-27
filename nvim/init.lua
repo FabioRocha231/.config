7c650b6e94eac13095d2cf07c8117d531f478b8f
FabioRocha231 <fabioharoldo1@gmail.com>
2022-08-27 20:29:21 -0300

    feat: more configs and updates

---
+++ nvim/init.lua
@@ -0,0 +1,32 @@
+require('base')
+require('highlights')
+require('maps')
+require('plugins')
+
+local has = function(x)
+  return vim.fn.has(x) == 1
+end
+local is_mac = has "macunix"
+local is_win = has "win32"
+
+if is_mac then
+  require('macos')
+end
+if is_win then
+  require('windows')
+end
+
+local rt = require('rust-tools')
+
+rt.setup({
+  server = {
+    on_attach = function(_, bufnr)
+      -- Hover actions
+      vim.keymap.set("n", "<C-space>", rt.hover_actions.hover_actions, { buffer = bufnr })
+      -- Code action groups
+      vim.keymap.set("n", "<Leader>a", rt.code_action_group.code_action_group, { buffer = bufnr })
+    end,
+  }
+})
+
+vim.g.ruby_host_prog = '/usr/local/lib/ruby/gems/3.1.0/bin/neovim-ruby-host'
